c666637852351c98a433b598cad950d1653ca13c
Michael Davidsaver <mdavidsaver@gmail.com>
2020-07-03 13:55:51 -0700

    add user encode/decode

--- src/server.cpp
+++ src/server.cpp
@@ -719,6 +719,7 @@ void Server::Pvt::onSearch(const UDPManager::Search& msg)
     if(nreply==0 && !msg.mustReply)
         return;
 
+    searchReply.clear();
     VectorOutBuf M(true, searchReply);
 
     M.skip(8, __FILE__, __LINE__); // fill in header after body length known
@@ -755,6 +756,7 @@ void Server::Pvt::doBeacons(short evt)
 {
     log_debug_printf(serversetup, "Server beacon timer expires\n%s", "");
 
+    beaconMsg.clear();
     VectorOutBuf M(true, beaconMsg);
     M.skip(8, __FILE__, __LINE__); // fill in header after body length known
 
