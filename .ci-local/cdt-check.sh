d54a4a82081e3c4c03b90891d66e11d28ee8ba0c
Michael Davidsaver <mdavidsaver@gmail.com>
2022-09-28 10:12:36 -0700

    update CDT test

--- .ci-local/cdt-check.sh
+++ .ci-local/cdt-check.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
 
-[ "$TRAVIS_OS_NAME" != "windows" ] || exit 0
-
 ret=0
 echo "Checking for GCC style static constructors or destructors"
-for ff in src/O.*/*.o ioc/O.*/*.o
+for ff in lib/*/lib*.a lib/*/lib*.so src/O.*/*.o ioc/O.*/*.o
 do
-    if nm "$ff" | grep __static_initialization_and_destruction
+    if nm "$ff" | grep -E '__static_initialization_and_destruction|_GLOBAL__sub'
     then
         echo "  Found in $ff"
         ret=1
