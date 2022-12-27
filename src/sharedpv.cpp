b17f8207676d12c48978c0af56ef1f16ace25afe
Michael Davidsaver <mdavidsaver@gmail.com>
2022-12-27 08:40:33 -0800

    sharedpv: avoid deadlock on error path
    Need to unlock for all MonitorSetupOp methods

--- src/sharedpv.cpp
+++ src/sharedpv.cpp
@@ -92,6 +92,7 @@ struct SharedPV::Impl : public std::enable_shared_from_this<Impl>
             self->subscribers.emplace(std::move(sub));
 
         }catch(std::exception& e){
+            UnGuard U(G);
             log_warn_printf(logshared, "%s Client %s: Can't attach() monitor: %s\n",
                             conn->name().c_str(), conn->peerName().c_str(), e.what());
             // not re-throwing for consistency
