e9ecf7e8dd130b2f6e0ec7b435440f6052e69222
Michael Davidsaver <mdavidsaver@gmail.com>
2023-09-05 11:31:41 +0200

    missing copyright boilerplate

--- src/pvxs/srvcommon.h
+++ src/pvxs/srvcommon.h
@@ -1,3 +1,8 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * pvxs is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
 #ifndef PVXS_SRVCOMMON_H
 #define PVXS_SRVCOMMON_H
 
