085a9019c482d0394b9ba8ad0d309f8274e0751d
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-08-01 08:48:46 +0100

    Adjusted version requirements based on Python version

--- setup.py
+++ setup.py
@@ -733,7 +733,7 @@ setup(
     #setup_requires = ['setuptools_dso'],
     # also need at runtime for DSO filename lookup
     install_requires = [
-        'setuptools_dso>=2.11a2',
+        'setuptools_dso>=2.7a1',
         epicscorelibs.version.abi_requires(),
     ],
     packages=['pvxslibs', 'pvxslibs.lib', 'pvxslibs.test'],
