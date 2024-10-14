dfd568e64edcae27b2fafd27289cb2cc941a01c2
Michael Davidsaver <mdavidsaver@gmail.com>
2024-10-13 19:26:45 -0700

    pvxvct IP range parsing

--- tools/pvxvct.cpp
+++ tools/pvxvct.cpp
@@ -64,7 +64,7 @@ parsePeer(const char *optarg)
             // only # of bits.  eg. "/24"
             std::istringstream strm(match[2].str());
             unsigned nbit=0;
-            if((strm>>nbit).good()) {
+            if(!(strm>>nbit).good() || nbit>32) {
                 throw std::runtime_error(pva::SB()<<"Expected number of bits.  not "<<match[2]);
             }
             mask.s_addr = htonl(0xffffffff<<(32-nbit));
