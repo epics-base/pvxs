f948a4fbb0eef30cfc0ef822a6bb6fa67d1fddb2
Michael Davidsaver <mdavidsaver@gmail.com>
2025-01-31 21:27:02 -0800

    client: log invalid monitor queueSize

--- src/clientmon.cpp
+++ src/clientmon.cpp
@@ -757,10 +757,17 @@ std::shared_ptr<Subscription> MonitorBuilder::exec()
 
     auto options = op->pvRequest["record._options"];
 
-    options["queueSize"].as<uint32_t>([&op](uint32_t Q) {
-        if(Q>1)
+    if(auto queueSize = options["queueSize"]) {
+        uint32_t Q = 0;
+        if(queueSize.as(Q) && Q>1) {
             op->queueSize = Q;
-    });
+        } else {
+            log_warn_printf(monevt, "%s requested invalid %s : %s\n",
+                            op->channelName.c_str(),
+                            op->pvRequest.nameOf(queueSize).c_str(),
+                            std::string(SB()<<queueSize).c_str());
+        }
+    }
 
     (void)options["pipeline"].as(op->pipeline);
 
