3c19b4c93320f8a73213c028c641270975746d05
george-mcintyre <george@level-n.com>
2025-02-03 09:21:35 +0100

    Update Docker image tag from 'pvxs_spva' to 'pvxs' in build script

--- example/docker/pvxs/build.sh
+++ example/docker/pvxs/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the Docker image and tag it
-docker build --no-cache -t <your_dockerhub_username>/pvxs_spva:latest .
+docker build --no-cache -t <your_dockerhub_username>/pvxs:latest .
 
 # Log in to DockerHub (only needed if not already logged in)
 docker login
 
 # Push the image to DockerHub
-docker push <your_dockerhub_username>/pvxs_spva:latest
+docker push <your_dockerhub_username>/pvxs:latest
