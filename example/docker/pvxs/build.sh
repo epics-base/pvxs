678bad0089b52bc994d8b50b16738f022932cd59
george-mcintyre <george@level-n.com>
2025-02-03 06:38:20 +0100

    Add --no-cache flag to Docker build commands in build scripts.

--- example/docker/pvxs/build.sh
+++ example/docker/pvxs/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image and tag it
-docker build -t <your_dockerhub_username>/pvxs_spva:latest .
+docker build --no-cache -t <your_dockerhub_username>/pvxs_spva:latest .
 
 # Log in to DockerHub (only needed if not already logged in)
 docker login
--- example/docker/spva_krb/build.sh
+++ example/docker/spva_krb/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image and tag it
-docker build -t <your_dockerhub_username>/spva_krb:latest .
+docker build --no-cache -t <your_dockerhub_username>/spva_krb:latest .
 
 # Log in to DockerHub (only needed if not already logged in)
 docker login
