bd6db89b0ce62082202e07380d224a6d9eb78c5d
george-mcintyre <george@level-n.com>
2025-02-12 10:49:04 +0100

    "Refactor Dockerfiles: modularize EPICS Base setup, update base images, and streamline dependencies."

---
+++ example/docker/epics-base/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the Docker image and tag it
+docker build --no-cache -t <your_dockerhub_username>/epics-base:latest .
+
+# Log in to DockerHub (only needed if not already logged in)
+docker login
+
+# Push the image to DockerHub
+docker push <your_dockerhub_username>/epics-base:latest
