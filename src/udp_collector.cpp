54231254be998a49a002483826810f8299c69dda
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-02-20 00:59:55 +0000

    Process requests targetting broadcast address

--- src/udp_collector.cpp
+++ src/udp_collector.cpp
@@ -390,6 +390,14 @@ void UDPCollector::process_one(const SockAddr &dest, const uint8_t *buf, size_t 
             for(auto L : listeners) {
                 if(L->searchCB && (L->dest.addr.isAny() || L->dest.addr==dest)) {
                     (L->searchCB)(*this);
+                } else if(L->searchCB && !(L->dest.addr.isAny())) {
+                    for(auto B : sock.broadcasts(&(L->dest.addr))) {
+                        if(!(B.compare(dest, false)) && (L->dest.addr.port() == dest.port())) {
+                            log_debug_printf(logio, "Processing broadcast %s on %s\n",
+                                dest.tostring().c_str(), L->dest.addr.tostring().c_str());
+                            (L->searchCB)(*this);
+                        }
+                    }                   
                 }
             }
 
