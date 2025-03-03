d6e3fc1dd9c3940b81bfd9c54a54663fbc8402a0
george-mcintyre <george@level-n.com>
2025-03-03 16:27:29 +0100

    "Update Docker build scripts and dependencies to use 'latest' tags and main branches. Replaced 'tls-dev' with 'latest' in image tags, base images, and PVXS git branch references for consistency."

--- example/docker/spva_krb/build.sh
+++ example/docker/spva_krb/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build the Docker image and tag it, login, then push it
-docker build --no-cache -t ${DOCKER_USERNAME}/spva_krb:tls-dev . && \
+docker build --no-cache -t ${DOCKER_USERNAME}/spva_krb:latest . && \
 docker login && \
-docker push ${DOCKER_USERNAME}/spva_krb:tls-dev
+docker push ${DOCKER_USERNAME}/spva_krb:latest
