b75eccbcd2efd0dc95158a57d1908af382617fb8
george-mcintyre <george@level-n.com>
2025-03-03 22:57:59 +0100

    "Update Docker image tags and dependencies to use 'tls-dev' instead of 'latest'"

--- example/docker/spva_std/build.sh
+++ example/docker/spva_std/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build the Docker image and tag it, login, then push it
-docker build --no-cache -t ${DOCKER_USERNAME}/spva_std:latest . && \
+docker build --no-cache -t ${DOCKER_USERNAME}/spva_std:tls-dev . && \
 docker login && \
-docker push ${DOCKER_USERNAME}/spva_std:latest
+docker push ${DOCKER_USERNAME}/spva_std:tls-dev
