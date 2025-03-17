018ea4ff38074617592a0944c9770cd2cb37c03f
George McIntyre <george@level-n.com>
2025-03-17 05:35:55 +0100

    Merge pull request #83
    * "Issue #60: Multi-platform builds

--- example/docker/spva_krb/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image and tag it, login, then push it
-docker build --no-cache --no-cache --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} --pull -t ${DOCKER_USERNAME}/spva_krb:latest . && \
-docker login && \
-docker push ${DOCKER_USERNAME}/spva_krb:latest
--- example/docker/spva_std/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image and tag it, login, then push it
-docker build --no-cache --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} --pull -t ${DOCKER_USERNAME}/spva_std:latest . && \
-docker login && \
-docker push ${DOCKER_USERNAME}/spva_std:latest
